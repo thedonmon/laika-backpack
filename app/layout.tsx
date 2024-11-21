e8bf26bfa32915952c9a1f7b7d2be1fce0388e6f
0xDon <22967202+thedonmon@users.noreply.github.com>
2024-11-21 11:47:59 -0700

    add meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,40 @@ import BackpackWalletProvider from "@/app/providers/BackpackWalletProvider";
 import "@/app/styles/wallet-adapter.css";
 import "@/app/globals.css";
 import { Toaster } from "@/components/ui/sonner"
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'The First Landing |  Eclipse × Backpack × $LAIKA',
+  description: 'Embark on an expedition to Eclipse to claim your LAIKA rewards.',
+  openGraph: {
+    title: 'The First Landing | Eclipse × Backpack × $LAIKA ',
+    description: 'Embark on an expedition to Eclipse to claim your LAIKA rewards.',
+    images: [
+      {
+        url: '/assets/background.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'The First Landing',
+      },
+    ],
+    type: 'website',
+    url: 'https://firstlanding.laika.is',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'The First Landing |  Eclipse × Backpack × $LAIKA',
+    description: 'Embark on an expedition to Eclipse to claim your LAIKA rewards.',
+    images: ['/assets/background.jpg'],
+    creator: '@laikaoneclipse',
+  },
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/icon.png', type: 'image/png' },
+    ],
+    apple: '/apple-icon.png',
+  },
+}
 
 export default function RootLayout({
   children,
